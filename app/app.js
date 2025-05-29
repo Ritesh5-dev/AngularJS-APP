ebb1b4b4357374c4bd0775cfc37affb82599e6b7
Ritesh5-dev <ritesh.j@cloudworkmates.com>
2025-05-29 12:09:44 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
 angular.module('myApp', [])
   .controller('MainCtrl', function($scope) {
-    $scope.greeting = 'Hello from AngularJS!, Welcome gggg';
+    $scope.greeting = 'Hello from AngularJS!, Welcome gggg123';
   });
